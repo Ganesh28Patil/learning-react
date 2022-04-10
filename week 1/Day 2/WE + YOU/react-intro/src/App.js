4970d640a5718c58f5ff2fd5bad987731462ae07
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-10 20:51:26 +0530

    mapping the object  & Component
    mapping the object in to the value so that it can be accessed properly.
    
    Introduction to Component
    And how to use component
    And how to make custom component

--- week 1/Day 2/WE + YOU/react-intro/src/App.js
+++ week 1/Day 2/WE + YOU/react-intro/src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const food = "Pizza";
+  const age = 12;
+  const skills = ["MERN","DSA","Design","Animations","Java","C#"];
+  const hobby = ["Cycling","Walking","Trecking","Swiming","Movie Time"];
   return (
     <div className="App">
        <input />
@@ -10,8 +14,44 @@ function App() {
      
       <img src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt='ImageMissing' />
   
+
+     <p>Your fav Food is {food} , you are {age} year old.</p>
+
+     <h1>You are {age > 18 ? " Eligible " : " Not Eligible "} for license</h1>
+
+     <div>
+       Your skills : 
+       <ul>
+         {
+           skills.map((e) =>{
+             return <li>{e}</li>;
+           })
+         }
+       </ul>
+     </div>
+     <div>
+       {skills.map((e) =>{
+        //  return <p className='redText'>Skill : {e}</p>
+          return <Skill  skill ={e} />;
+       })}
+     </div>
+
+     <span>
+       {
+         hobby.map((ele)=>{
+           return <Parameter value ={ele}/>
+         })
+       }
+     </span>
     </div>
   );
 }
 
+function Skill({skill}){
+  return <p className = 'redText'>Skill : {skill}</p>
+}
+
+function Parameter({value}){
+  return <p className = 'blueWord'>Hobby : {value} </p>
+}
 export default App;
