0febc947639733da1d1836cfa5a3f0f48652d468
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-10 16:06:31 +0530

    append h1 using react
    append h1 using react.creatElement("h1",{ className:"txt","hello React"});
    then render it on reactDom.render();

--- week 1/Day 2/WE + YOU/webpack/src/index.js
+++ week 1/Day 2/WE + YOU/webpack/src/index.js
@@ -1,5 +1,7 @@
 import {add, product} from "./calc";
 
+import React from "react";
+import ReactDOM from "react-dom";
 import logo from "./file.png";
 
 import("./index.css");
@@ -19,6 +21,20 @@ image.alt="ReactIcon";
 document.getElementById("banner").append(image);
 */
 
+const h2 = React.createElement("h2",{id:"text"},"Hello Ganesh");
+const p = React.createElement("p",{className:"blueText"},h2,"Wellcome to React Class");
+ReactDOM.render(p,document.getElementById("txt1"));
+
+const h1 = React.createElement("h1",{className:"redText"},"Hello React .....!");
+
+ReactDOM.render(h1, // what
+    document.getElementById("txt")  // where
+    );
+
 const root = document.getElementById("root");
 
+ReactDOM.render(
+    "Hello React", // what
+    root // whaere
+);
 
