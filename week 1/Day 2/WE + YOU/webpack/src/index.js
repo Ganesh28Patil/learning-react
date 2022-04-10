7f3c0d9581765708b93f222e38ab18ae48eb7c48
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-10 17:50:58 +0530

    React in new way JSX
    JSX is done in react
    we need to take care of .bablerc
    JSX -> javascript XML

--- week 1/Day 2/WE + YOU/webpack/src/index.js
+++ week 1/Day 2/WE + YOU/webpack/src/index.js
@@ -2,11 +2,15 @@ import {add, product} from "./calc";
 
 import React from "react";
 import ReactDOM from "react-dom";
+
 import logo from "./file.png";
 
 import("./index.css");
+
 console.log(add(10,9));
 console.log(product(5,6));
+// basic way to write DOM code
+
 /*
 const h1 = document.createElement("h1");
 h1.innerText = "wellcome to web pack";
@@ -20,10 +24,19 @@ image.src = logo;
 image.alt="ReactIcon";
 document.getElementById("banner").append(image);
 */
+// basic React way to write ReactDOM code
+
+/*
+// const flag = false;
+
+const flag = true;
+const br = React.createElement("br",{},);
 const img = React.createElement("img",{src:"./src/file.png",alt:"ReactImg"})
 const image = React.createElement("img",{width:"500px",height:"300px",src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlGMFjiKISnkW40QqO2C5zxKWLFs-L-9RTCg&usqp=CAU"})
 const h2 = React.createElement("h2",{id:"text"},"Hello Ganesh");
-const p = React.createElement("p",{className:"blueText"},h2,img,"Wellcome to React Class",image);
+const p = React.createElement("p",{className:"blueText"},h2,img,"Wellcome to React Class",br,flag ? image: null);
+
+
 ReactDOM.render(p,document.getElementById("txt1"));
 
 const h1 = React.createElement("h1",{className:"redText"},"Hello React .....!");
@@ -38,4 +51,22 @@ ReactDOM.render(
     "Hello React", // what
     root // whaere
 );
+
+const th = React.createElement("th",{className:"cell"})
+const tr = React.createElement("tr",{className:"row"},th,th,th,th);
+const table = React.createElement("table",{},tr,tr,tr);
+ReactDOM.render(table,document.getElementById("tBody"));
+*/
+
+// anternate way to write ReactDOM code using jsx
+
+ 
+ReactDOM.render(
+    <div>
+        <h1>Hello React </h1>
+        <img src="https://docs.microsoft.com/en-us/windows/images/react-native-android-emulator.png" height="500px" width="300px"/>
+    </div>, // What: JSX
+    document.getElementById("newRoot") // where
+);
+
 
--- week 1/Day 2/WE + YOU/webpack/webpack.config.js
+++ week 1/Day 2/WE + YOU/webpack/webpack.config.js
@@ -10,9 +10,12 @@ module.exports = {
     },
     mode:"development",
     module:{
-        rules:[{test:/\.css$/,use:["style-loader","css-loader"],}, 
-        {test: /\.(png|jpe?g|gif)$/i,use: [{loader: 'file-loader',},],},
-   /* { test: /\.(png|svg|jpg|jpeg|gif)$/i,type: 'asset/resource', },*/]
+        rules:[
+            {test:/\.css$/,use:["style-loader","css-loader"],},
+            {test:/\.js$/,use:["babel-loader"],},
+            {test: /\.(png|jpe?g|gif)$/i,use: [{loader: 'file-loader',},],},
+            /* { test: /\.(png|svg|jpg|jpeg|gif)$/i,type: 'asset/resource', },*/
+        ]
     },
    
 }
