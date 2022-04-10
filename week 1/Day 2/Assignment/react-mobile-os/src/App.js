7d6f766996a753b953fa4a990975db7dedaae8a9
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-11 02:34:43 +0530

    almost done

--- week 1/Day 2/Assignment/react-mobile-os/src/App.js
+++ week 1/Day 2/Assignment/react-mobile-os/src/App.js
@@ -1,8 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+  const mos = ["Andriod","Blackberry","iPhone","Windows Phone"];
+  const mm = ["Samsung","HTC","Micromax","Apple"];
+  const array = ["Andriod","Blackberry","iPhone","Windows Phone"];
 
 function App() {
   const headPoint = ["Operating System","Manufacturers"];
+
   // const mos = ["Andriod","Blackberry","iPhone","Windows Phone"];
   // const mm = ["Samsung","HTC","Micromax","Apple"];
 
@@ -11,19 +15,45 @@ function App() {
 
       <div>
         {
-            headPoint.map((e)=>{ 
-             return <Heading value = {e}/>      
+            headPoint.map((e)=>{              
+             return  <Heading value = {e} />      
               })
         }
       </div>
      
-    
+      {/* <div>
+        {
+          mos.map((e)=>{
+            return  <Listmos d ={e} /> 
+          })          
+        }
+      </div>
+      
+      <div>
+        {
+          mm.map((e)=>{
+            return  <Listmm t ={e} /> 
+          })          
+        }
+      </div> */}
       
     </div>
   );
 }
-function Heading({d}){
-  return <h1>Mobile {d}</h1>
+function Heading({value}){
+  return <p> <h1> Mobile {value}  </h1> 
+  {
+    array.map((e)=>{
+      return <ul> <Listmos d ={e} /> </ul>
+    })          
+  }
+</p>;
+ 
 }
-
+function Listmos({d}){
+  return <li>{d}</li>
+}
+// function Listmm({t}){
+//   return <li>{t}</li>
+// }
 export default App;
