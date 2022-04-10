4cf5c030d99e03225c2e68614eeff63f04e87f64
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-10 19:16:54 +0530

    create-react-app app-name
    craeting react app using
    
    npm i -g create-react-app
    
    create-react-app --version
    if it show 5.00 it means
    
    react is working
    
    and we will work on app.js in SRC folder

---
+++ week 1/Day 2/WE + YOU/react-intro/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <input />
+      <button>Search</button>
+     
+     
+      <img src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt='ImageMissing' />
+  
+    </div>
+  );
+}
+
+export default App;
