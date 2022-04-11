79cce739bf23e6f0b7b236140dc1634893fe3db7
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-11 17:14:29 +0530

    counter using react.useState()
    React.useState(0) gives 2 values [value,setValue]
    
    so here we get 2 values as [counter,setCounter]
    
    const [counter,setCounter] = React.useState(0);
    
    and zero(0) means we are starting our counter from zero.
    and
    
    setCOunter is the call-Back function.
    
    HandleIncrement function will takecare of  increase of 1 & Decrease of 1
    
    Note: In create onClick is written in Camel case whereas in HTML-CSS we write in small case

--- week 1/Day 3/preclass hooks-usestate-props/preclass-day2/src/App.js
+++ week 1/Day 3/preclass hooks-usestate-props/preclass-day2/src/App.js
@@ -1,17 +1,32 @@
 // import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  const [counter,setCounter] = React.useState(0);
+// destructuring
+// REact.useState = > [ value , setValue ]
+  const handleIncrement = (value) =>{
+    setCounter(counter + value);
+    // console.log('counter:', counter);
+  // React needs you to use setCounter
+  // so that it knows that the state is upatded
+  }
   return (
     <div className="App">
       <p>hello masai</p>
       <h1>Counter</h1>
-      <h2>0</h2>
-      <button>ADD</button>
-      <button>REDUCE</button>
-
+      <h2>{counter}</h2>
+      <button onClick={() => handleIncrement(1)}>ADD</button>
+      <button onClick={() => handleIncrement(-1)}>REDUCE</button>
     </div>
   );
 }
 
 export default App;
+
+// state,
+// props
+// hooks
+
+// state is something that the component remembers
