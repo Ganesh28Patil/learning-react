c49b9ff260191f036ff0d5008db110abe9b6d8e5
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-11 12:39:48 +0530

    some changes

--- Evaluation/sprint-1/cricket-score-tracker/src/App.js
+++ Evaluation/sprint-1/cricket-score-tracker/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
         <h1 className="scoreCount">
           {
             // show "score" here
+            
           }
         </h1>
       </div>
@@ -38,21 +39,21 @@ function App() {
     <div className="addScore">
       Add Score
       {/* these buttons should add the respective amount in the score */}
-      <button className="addScore1">Add 1</button>
-      <button className="addScore4">Add 4</button>
-      <button className="addScore6">Add 6</button>
+      <button className="addScore1 space">Add 1</button>
+      <button className="addScore4 space">Add 4</button>
+      <button className="addScore6 space">Add 6</button>
     </div>
 
     <div className="addWicket">
       Add Wicket
       {/* Increase the count of wicket */}
-      <button>Add 1 wicket</button>
+      <button className='space'>Add 1 wicket</button>
     </div>
 
     <div className="addBall">
       Add ball
       {/* Increase the total number of balls thrown here. */}
-      <button>Add 1</button>
+      <button className='space'>Add 1</button>
     </div>
 
     {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
