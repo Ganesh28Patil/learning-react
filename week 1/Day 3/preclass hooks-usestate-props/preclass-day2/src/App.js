791b211dd09dadb51873adbc211fc08a236ab1ff
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-11 16:32:05 +0530

    preeclass practice

---
+++ week 1/Day 3/preclass hooks-usestate-props/preclass-day2/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello masai</p>
+      <h1>Counter</h1>
+      <h2>0</h2>
+      <button>ADD</button>
+      <button>REDUCE</button>
+
+    </div>
+  );
+}
+
+export default App;
