bb6d5ca961a6d58e76cc98f7e44d7b040dd775ee
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-16 15:12:34 +0530

    O(n) Next greater element
    O(n) time complexity for
    Next greater element

--- week 1/Day 1/DSA/nextGreaterElement.js
+++ week 1/Day 1/DSA/nextGreaterElement.js
@@ -5,12 +5,42 @@ var data = [1,3,2,4];
 // i/p 1 3 2 4
 // o/p 3 4 4 -1
 //indx 0 1 2 3
+
+// O(n) time complexity
+let stack = [];
+let ans = [];
+
+for(let i = data.length; i >= 0; i--){
+    if(stack.length == 0){
+        ans.push(-1);
+    }else if(stack.length > 0 && stack[stack.length-1] >= data[i]){
+        ans.push(stack[stack.length - 1]);
+    }else if(stack.length > 0 && stack[stack.length-1] <= data[i]){
+        while(stack.length > 0 && stack[stack.length - 1] <= data[i]){
+            stack.pop();
+        }
+        if(stack.length === 0){
+            ans.push(-1);
+        }else{
+            ans.push(stack[stack.length-1]);
+        }
+    }
+    stack.push(data[i]);
+
+}
+console.log(ans.reverse().join(" "));
+
+
+
+
+/*
+// O(n^2)
 let ans = [];
 for(var i = 0; i < data.length; i ++){
-    console.log("i = ",i);
+    // console.log("i = ",i);
     let next = data[i];
     for(var j = i; j < data.length; j++){
-        console.log("i",i,"  ","j",j,"  ","Data/next",next);        
+        // console.log("i",i,"  ","j",j,"  ","Data/next",next);        
         if(data[j+1] > next){
             next = data[j+1];
             ans.push(next);
@@ -28,6 +58,8 @@ for(var i = 0; i < data.length; i ++){
 
 
 console.log("ans: ",ans.join(" "));
+
+*/
 
 
 
