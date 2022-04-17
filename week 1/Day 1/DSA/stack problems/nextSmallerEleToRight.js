1a5933e8a6f0aff0f2290170e718dde53f0d1e7a
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-17 16:15:27 +0530

    stack problems
    next Greater Element to Right
    next Greater Element to Left
    Nearest Smaller to Left
    Nearest Smaller to Right

---
+++ week 1/Day 1/DSA/stack problems/nextSmallerEleToRight.js
@@ -0,0 +1,30 @@
+// Nearest Smaller to Right
+// i/p  4 5  2 10 8
+// o/p -1 4 -1  2 2
+//indx  0 1  2  3 4
+
+var arr = [4,5,2,10,8];
+
+let stack = [];
+let ans = [];
+
+for(let i = arr.length-1; i >=0; i--){
+    if(stack.length == 0){
+        ans.push(-1);
+    }else if(stack.length > 0 && stack[stack.length-1] < arr[i]){
+        ans.push(stack[stack.length-1]);
+    }else if(stack.length > 0 && stack[stack.length-1] >= arr[i]){
+        while(stack.length > 0 && stack[stack.length-1] >= arr[i]){
+            stack.pop();
+        }
+        if(stack.length === 0){
+            ans.push(-1);
+        }else{
+            ans.push(stack[stack.length-1]);
+        }
+    }
+    stack.push(arr[i]);
+}
+console.log(ans.reverse().join(" "));
+        
+
