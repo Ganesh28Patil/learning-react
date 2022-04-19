d473e1cec632709dea889afa6e2049704319be8c
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-20 04:34:23 +0530

    Challenge 2
    let date = new Date().toLocaleDateString();
    let time = new Date().toLocaleTimeString();

--- react_tech-thappa/day1/basic-challenge1/src/index.js
+++ react_tech-thappa/day1/basic-challenge1/src/index.js
@@ -6,10 +6,11 @@ import './index.css';
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+// Challenge 1
 
 ReactDOM.render(
  <>
+ <h4>Challenge 1</h4>
  <h1>Thappa Technical Netflix Pick List</h1>
  <p>List of 5 Best Series :-</p>
  <ol>
@@ -20,5 +21,23 @@ ReactDOM.render(
  <li>Picky Binder</li>
  </ol>
  </>
-,document.getElementById('root'));
+,document.getElementById('task1'));
 
+
+
+
+// Challenge 2
+const fName = "Ganesh";
+const lName = "Patil"
+let date = new Date().toLocaleDateString();
+let time = new Date().toLocaleTimeString();
+
+
+ReactDOM.render(
+<>
+ <h4>Challenge 2</h4>
+ <h1>{` My Name is ${fName} ${lName} `}</h1>
+ <p>todays Date is {date}</p>
+ <p>Current Time is {time}</p>
+
+</>,document.getElementById('task2'));
