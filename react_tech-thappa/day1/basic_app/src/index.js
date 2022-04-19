d1b785aed72701a098918dd7b380cc7bedaf74d8
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-20 03:43:10 +0530

    expression in jsx
    js expression is given in curly brackets
     { }
    
    and we cannot write condition statement inside this  { }  we have to write if else statement in Ternary Operators like  : ?

--- react_tech-thappa/day1/basic_app/src/index.js
+++ react_tech-thappa/day1/basic_app/src/index.js
@@ -1,3 +1,4 @@
+import matchers from '@testing-library/jest-dom/matchers';
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
@@ -9,12 +10,22 @@ root.render(
  <h1>Hello World</h1>,root
 );
 root2.render([<h1>hello React</h1>,<p>Learn & Apply</p>],root2);
-
+// React.fragment
 ReactDOM.render(
 <>
 <h1>React.fragment</h1>
 <p>it is an alternate for array in jsx</p>
 </>,document.getElementById("root3"));
+
+// expressions in react 
+
+const firstName = "Ganesh";
+ReactDOM.render(
+<>
+  <h1>My name is {firstName} Patil</h1>
+  <p>Check here  Random Number is {Math.floor((Math.random()*100))}</p>
+</>
+,document.getElementById("txt"));
 
 
 // jsx => javascript xml
