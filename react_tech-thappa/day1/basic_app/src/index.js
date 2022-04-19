ddc15aa8a062fab4df580d460de611b5430f69a8
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-20 02:34:52 +0530

    rendering on ReactDOM
    u can see how we can make use of array in react
    
    use of array is replace using react.fragment
    
    sugercode for react.fragment is <></>

--- react_tech-thappa/day1/basic_app/src/index.js
+++ react_tech-thappa/day1/basic_app/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
 root.render(
  <h1>Hello World</h1>,root
 );
+root2.render([<h1>hello React</h1>,<p>Learn & Apply</p>],root2);
+
+ReactDOM.render(
+<>
+<h1>React.fragment</h1>
+<p>it is an alternate for array in jsx</p>
+</>,document.getElementById("root3"));
+
+
+// jsx => javascript xml
+// jsx => javascript extension
