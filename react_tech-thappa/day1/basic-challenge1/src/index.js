0219437a29395f571f1f42533f104be14eb9aeaa
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-20 03:27:40 +0530

    basic challenge
    challenge is like assignment

---
+++ react_tech-thappa/day1/basic-challenge1/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // /client';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
+ <>
+ <h1>Thappa Technical Netflix Pick List</h1>
+ <p>List of 5 Best Series :-</p>
+ <ol>
+ <li>Dark</li>
+ <li>Break Bad</li>
+ <li>Narcos</li>
+ <li>Lost in Space</li>
+ <li>Picky Binder</li>
+ </ol>
+ </>
+,document.getElementById('root'));
+
