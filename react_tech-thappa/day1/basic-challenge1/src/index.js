33cf949c83ed00c5cdc1fb3b32b26e02fbf52337
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-20 16:40:50 +0530

    Challenge 3
    react js mini app
    In this we will change the text statement dynamically and we will  change the color dynamically

--- react_tech-thappa/day1/basic-challenge1/src/index.js
+++ react_tech-thappa/day1/basic-challenge1/src/index.js
@@ -21,7 +21,7 @@ ReactDOM.render(
  <li>Picky Binder</li>
  </ol>
  </>
-,document.getElementById('task1'));
+,document.getElementById('challenge1'));
 
 
 
@@ -40,4 +40,33 @@ ReactDOM.render(
  <p>todays Date is {date}</p>
  <p>Current Time is {time}</p>
 
-</>,document.getElementById('task2'));
+</>,document.getElementById('challenge2'));
+
+// Challenge 3
+
+let hour = new Date(2020,5,5,22);
+ hour = hour.getHours();
+ let text = "";
+
+ let txtStyle = {};
+ if(hour > 1 && hour < 12){
+     text = "Morning";
+     txtStyle.color="green"
+ }else if(hour >= 12 && hour < 19){
+     text = "Afternoon";
+     txtStyle.color="red"
+ }else{
+     text = "Night";
+     txtStyle.color="Blue"
+ }
+ReactDOM.render(
+    <>
+     <h4>Challenge 3</h4>
+    <div className="box"> 
+       
+        <h1 className='headTxt'>Hello, Good <span style={txtStyle}> {text} </span></h1>
+    </div>
+    </>,document.getElementById('challenge3')
+)
+
+
