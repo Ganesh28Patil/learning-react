fcf08ca3d1db161c64719ec58711e391f1dcbbbd
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-21 01:55:14 +0530

    check once may be inccorect
    may be inccorect
    
    will upatde the code in next commmit.

--- react_tech-thappa/day1/basic_app3_importexport/src/App.jsx
+++ react_tech-thappa/day1/basic_app3_importexport/src/App.jsx
@@ -1,3 +1,11 @@
 const text = "Thappa Technical";
+const favProg = 'React';
+const sample = 'Sample code'
+
+function myName(){
+let name = "Ganesh Patil";
+return name;
+}
+export {favProg,sample,myName};
 
 export default text;
--- react_tech-thappa/day1/basic_app3_importexport/src/index.js
+++ react_tech-thappa/day1/basic_app3_importexport/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  kuchbhi  from './App';
+import  kuchbhi, {favProg,sample,myName} from './App';
+import * as ques from './App' ;
 
 // ES6 Modules Import Export in React JS
 
@@ -10,6 +11,10 @@ ReactDOM.render(
 <>
 <ol>
   <li>{kuchbhi}</li>
+  <li>{ques.default}</li>
+  <li>{favProg}</li>
+  <li>{sample}</li>
+  <li>{myName}</li>
 </ol>
 </>
 ,document.getElementById('root'));
