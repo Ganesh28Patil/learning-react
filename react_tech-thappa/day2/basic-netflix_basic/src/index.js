067d14b3d6cfd75e9ccca5ee0a8c5de4cc97f505
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-22 13:06:56 +0530

    netflix part 3
    map will pass the object from array
    one by one.
    
    here component is passed in simple function call and
    Fat Arrow function

--- react_tech-thappa/day2/basic-netflix_basic/src/index.js
+++ react_tech-thappa/day2/basic-netflix_basic/src/index.js
@@ -8,6 +8,17 @@ import Cards from './Cards';
 import Sdata from './sData';
 // console.log('Sdata:', Sdata,Sdata.length)
 
+// function ncard(val){
+//   // console.log(val);
+//   return(
+//     <Cards 
+//       imgscr = {val.imgscr} title = {val.title}
+//       sname = {val.sname} links= {val.links}
+//     />
+//   );
+// }
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,36 +26,15 @@ root.render(
  
  <>
     <h1 className='heading_style'>List of Top 5 Netflix Series in 2020</h1>
-
-    <Cards  
-      imgscr= "https://wallpapercave.com/wp/wp4056410.jpg"
-      sname = "DARK"
-      title = "Netflix Original Series"
-      links = "https://www.netflix.com/in/title/89990668?source=35" />
-    <Cards 
-      sname  =   "Extra Curricular"
-      imgscr = "https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-      title  =  "Netflix original Series"
-      links  =  "https://www.netflix.com/search?q=extra%20curricular&jbv=80990668"/>
-    <Cards 
-      sname  = "Stranger Things" 
-      imgscr = "https://i.pinimg.com/originals/95/64/cc/9564cc5d91b06c8705bb95ea22b9e940.png" 
-      title  = "Netflix Original Series" 
-      links  = "https://www.netflix.com/in/title/89990668?source=35"
-    />
-    <Cards sname ={Sdata[0].sname} imgscr = {Sdata[0].imgscr} 
-           title = {Sdata[0].title} links = {Sdata[0].links}
-     />
-     <Cards sname ={Sdata[1].sname} imgscr = {Sdata[1].imgscr} 
-           title = {Sdata[1].title} links = {Sdata[1].links}
-     />
-     <Cards sname ={Sdata[2].sname} imgscr = {Sdata[2].imgscr} 
-           title = {Sdata[2].title} links = {Sdata[2].links}
-     />
-     <Cards sname ={Sdata[3].sname} imgscr = {Sdata[3].imgscr} 
-           title = {Sdata[3].title} links = {Sdata[3].links}
-     />  
-  
+        {/* {Sdata.map(ncard)}   */}
+        {Sdata.map((val,index) =>{
+          return (
+            <Cards
+                imgscr = {val.imgscr} title = {val.title}
+                sname = {val.sname} links= {val.links}
+            />
+          )
+        })}
  </>
 
  
--- react_tech-thappa/day2/basic-netflix_basic/src/sData.jsx
+++ react_tech-thappa/day2/basic-netflix_basic/src/sData.jsx
@@ -36,11 +36,11 @@ const Sdata = [
         links  :  "https://www.netflix.com/in/title/89990668?source=35",
     },
     {
-    sname: "the Vampire Diaries ", 
-    imgscr: "https://images.justwatch.com/poster/8589251/5592", 
-    title: "azon Original Series", 
-    links: "https://www.netflix.com/in/title/80990668?source=35",
-    },
+        sname: "The Vampire Diaries", 
+        imgscr: "https://images.justwatch.com/poster/8589251/s592", 
+        title: "azon Original Series", 
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+        },
     {
     sname: "My First-2 Love", 
     imgscr:"https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg", 
