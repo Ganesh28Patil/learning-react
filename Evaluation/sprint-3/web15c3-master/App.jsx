bbb0605f3d1c480c7c50ace518850f0c22cdd5e8
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-25 17:02:40 +0530

    ..

--- Evaluation/sprint-3/web15c3-master/App.jsx
+++ Evaluation/sprint-3/web15c3-master/App.jsx
@@ -3,7 +3,7 @@ import { Login } from "./components/Login";
 import { EmployeeList } from "./components/EmployeeList";
 import { EmployeeDetails } from "./components/EmployeeDetails";
 import { Admin } from "./components/Admin";
-import { ProtectedRoute } from "./components/PrivateRoute";
+// import { ProtectedRoute } from "./components/PrivateRoute";
 import { Navbar } from "./components/Navbar";
 import { Logout } from "./components/Logout";
 
