5692e3f4b93357d6eb847d824b31cd81cc5667b0
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-28 14:25:29 +0530

    practice part
     for practice of baisc form concept

--- react_tech-thappa/day4/basic-form1/src/App.js
+++ react_tech-thappa/day4/basic-form1/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
@@ -23,4 +23,29 @@ function App() {
   );
 }
 
-export default App;
+// for practice of baisc form concept
+const App2 = () =>{
+  const [name, setName] = useState();
+  const [fullname,setFullName] = useState();
+  const addText = (e) => {
+    setName(e.target.value);
+  }
+  const send = () =>{
+    setFullName(name);
+  }
+return (
+
+  <>
+    <div>
+      <h1> hello {fullname}</h1>
+      <input type="text" placeholder="Enter Name" onChange={addText} />
+      <button onClick={ send}>Send</button>
+    </div>
+  </>
+)
+
+}
+
+export {App, App2};
+
+
--- react_tech-thappa/day4/basic-form1/src/index.js
+++ react_tech-thappa/day4/basic-form1/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App,App2} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App2 />   {/* practice part*/}
   </React.StrictMode>
 );
 
